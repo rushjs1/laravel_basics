6485bc821490f829604db9b71df0bfb6cfd4a596
John Rush <john.rush@jacksonsystems.com>
2022-01-10 14:03:43 -0500

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
        // Paginator::useBootstrap(); 
+       if(env('APP_ENV') !== 'local'){
+           URL::forceScheme('https');
+       }
     }
 }
